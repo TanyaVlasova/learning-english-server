36684c2063b48b54a15f36c02000183cf32049f6
Tatyana Vlasova <vlasova.t@divan.ru>
2024-05-04 22:00:00 +0300

    change host

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
   password: "4003563",
-  host: "79.174.93.38",
+  host: "89.111.173.123",
   port: 5432,
   database: "learning_english",
 });
